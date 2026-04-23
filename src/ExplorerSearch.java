0848c2e6096763cb85727ea31bda26514bfd6cf9
f00d <delossantos.orion@student.greenriver.edu>
2026-04-22 19:57:48 -0700

    most of the algorithm done

--- src/ExplorerSearch.java
+++ src/ExplorerSearch.java
@@ -32,6 +32,83 @@ public class ExplorerSearch {
         // Implement your method here!
         // Please also make more test cases
         // I STRONGLY RECOMMEND testing some helpers you might make too
-        return -1;
+
+        boolean[][] visited = new boolean[island.length][island[0].length];
+        reachableArea(island, explorerLocation(island), visited);
+        int count = 0;
+
+        for (boolean[] arr : visited){
+            for(int i = 0; i < arr.length; i++){
+                if(arr[i] == true){
+                    count++;
+                }
+            }
+        }
+        return count;
+
+    }
+    public static void reachableArea(int[][] island, int[] loc, boolean[][] visited) {
+        int currentRow = loc[0];
+        int currentColumn = loc[1];
+        if(island[currentRow][currentColumn] == 1)return;
+        if(visited[currentRow][currentColumn]) return;
+
+        visited[currentRow][currentColumn] = true;
+
+        List<int[]> validLocs = possibleMoves(island, loc);
+        for(int[] move: validLocs){
+            reachableArea(island, move, visited);
+        }
+
+    }
+
+
+    public static int[] explorerLocation(int[][] island){
+        int count = 0;
+
+        for (int[] arr : island){
+            for(int i = 0; i < arr.length; i++){
+                if(arr[i] == 0){
+                    return new int[]{count, i};
+                }
+            }
+            count++;
+        }
+
+        throw new IllegalArgumentException("No explorer present");
+    }
+
+    public static List<int[]> possibleMoves(int[][] island, int[] location){
+        int currentRow = location[0];
+        int currentColumn = location[1];
+        List<int[]> validLocs = new ArrayList<>();
+
+        //up
+        int newR = currentRow-1;
+        int newC = currentColumn;
+        if(newR >= 0 && island[newR][newC] != 3 && island[newR][newC] != 2){
+            validLocs.add(new int[]{newR, newC});
+        }
+        //down
+        newR = currentRow+1;
+        newC = currentColumn;
+        if(newR < island.length && island[newR][newC] != 3 && island[newR][newC] != 2){
+            validLocs.add(new int[]{newR, newC});
+        }
+        //right
+        newR = currentRow;
+        newC = currentColumn+1;
+        if(newC < island[0].length && island[newR][newC] != 3 && island[newR][newC] != 2){
+            validLocs.add(new int[]{newR, newC});
+        }
+        //left
+        newR = currentRow;
+        newC = currentColumn-1;
+        if(newC >= 0 && island[newR][newC] != 3 && island[newR][newC] != 2){
+            validLocs.add(new int[]{newR, newC});
+        }
+        return validLocs;
+
+
     }
 }
--- src/ExplorerSearchTest.java
+++ src/ExplorerSearchTest.java
@@ -1,4 +1,9 @@
 import static org.junit.Assert.*;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 import org.junit.Test;
 
 public class ExplorerSearchTest {
@@ -17,4 +22,56 @@ public class ExplorerSearchTest {
 
     // Add more tests here!
     // Come up with varied cases
+    @Test
+    public void testExplorerLocation_centerOfGrid() {
+        int[][] island = {
+            {1, 1, 1},
+            {1, 0, 1},
+            {1, 1, 1}
+        };
+        int[] expected = {1, 1};
+        assertArrayEquals(expected, ExplorerSearch.explorerLocation(island));
+    }
+
+    @Test
+    public void testPossibleMoves_oneOpen_twoWalls_oneEdge() {
+        // Salamander at (1, 1)
+        // Up is W, down is wall, left is edge (0), right is open
+        int[][] island = {
+            {3, 3, 3},
+            {1, 0, 1},
+            {3, 3, 3}
+        };
+        int[] location = {1, 1};
+        List<int[]> moves = ExplorerSearch.possibleMoves(island, location);
+        Set<String> moveSet = toSet(moves);
+
+        assertEquals(2, moves.size());
+        assertTrue(moveSet.contains("1,2"));
+        assertTrue(moveSet.contains("1,0"));
+    }
+    @Test
+    public void testPossibleMovesLarge() {
+        // Salamander at (1, 1)
+        // Up is W, down is wall, left is edge (0), right is open
+        int[][] island = {
+            {1,1,1,3,1,1},
+            {3,2,3,1,3,1},
+            {1,1,1,1,3,3},
+            {3,1,2,1,0,1},
+            {1,1,1,2,1,1},
+        };
+        int[] location = {3,4};
+        List<int[]> moves = ExplorerSearch.possibleMoves(island, location);
+        Set<String> moveSet = toSet(moves);
+
+        assertEquals(3, moves.size());
+    }
+    private Set<String> toSet(List<int[]> list) {
+        Set<String> set = new HashSet<>();
+        for (int[] arr : list) {
+            set.add(arr[0] + "," + arr[1]);
+        }
+        return set;
+    }
 }
